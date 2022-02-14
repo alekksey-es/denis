30c81998ae4091cc126721b35836d06ec89bbcff
Denis Huk <denis.hooke@gmail.com>
2022-02-15 02:02:13 +0300

    Fixed routes for React Router

--- site/src/components/AppRouter.js
+++ site/src/components/AppRouter.js
@@ -1,30 +1,30 @@
 import React, {Component, useContext} from 'react';
 import {authRoutes, publicRoutes} from "../routs";
-import {MARKET_ROUTE} from "../utils/consts";
-import {Routes, Route, Navigate} from 'react-router-dom';
+import {Routes, Route, } from 'react-router-dom';
 import {Context} from "../index";
-import Market from "../pages/Market";
+
 const AppRouter = () => {
     const {user} = useContext(Context)
+    let routes = getRoutes(publicRoutes);
 
-    console.log(user)
-    let routes = []
-    // if(user.isAuth){
-    //     routes =  authRoutes.map(({path, Component}) =>
-    //         <Route key={path} path={path} component={Component} exact />
-    //     )
-    // }
-    // const publicRouts =publicRoutes.map(({path, Component}) =>{
-    //    console.log(path)
-    // return <Route key={path} path={path} component={Component} exact />
-    // })
-    // routes = routes.concat(publicRoutes)
+    if (user.isAuth) {
+        routes = routes.concat(getRoutes(authRoutes));
+    }
 
     return (
         <Routes>
-            <Route path='/market' render={() => <div>Home</div>} />
-            {/*<Navigate to={MARKET_ROUTE}/>*/}
+            {routes}
         </Routes>
     );
+
+    function getRoutes(list) {
+        if (!list) {
+            return [];
+        }
+
+        return list.map(({path, Component}) => (
+            <Route key={path} path={path} element={<Component/>} exact/>
+        ));
+    }
 };
 export default AppRouter;
